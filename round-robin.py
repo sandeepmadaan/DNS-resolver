e5c4875e5d52149524297cd49b6d67fb7108f2a4
sandy <sandy@kaur-sandeep>
2014-07-06 22:30:37 +0530

    first commit

---
+++ round-robin.py
@@ -0,0 +1,6 @@
+from itertools import chain, izip_longest
+
+def roundrobin(*iterables):
+    sentinel = object()
+    return (x for x in chain(*izip_longest(fillvalue=sentinel, *iterables)) if x is not sentinel)
+print list(roundrobin('ABC', 'D', 'EF'))
